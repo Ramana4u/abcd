435a32436e4c0d8cf52ea58d8d1027333b5e4950
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-07 09:11:43 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
-#!/bin/bash\n
-sudo -i\n
-apt-get update -y\n
-apt-get upgrade -y\n
-apt-get install apache2 -y\n
-apt-get install awscli -y\n
-cd /var/www/html\n
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install awscli -y
+sudo apt-get install apache2 -y
+cd /var/www/html
+sudo chmod -R 777 /var/www/html
 aws s3 cp s3://arn:aws:s3:us-east-2:697613968254:accesspoint/accesspoint11/index.html index11.html

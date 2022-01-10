b283c591ea045ae29aa148c656067e76c2c76410
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-10 11:25:51 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,5 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install awscli -y
 sudo apt-get install apache2 -y
-cd /var/www/html
 sudo chmod -R 777 /var/www/html
- aws s3 cp s3://demo-s3-697613968254/index11.html index.html
+ aws s3 cp s3://demo-s3-697613968254/index11.html /var/www/html/index.html

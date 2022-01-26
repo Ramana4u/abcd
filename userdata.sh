729b6f66fd94c3c8920b8cd4e14ce4d3711e70a8
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-26 20:39:03 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo wget https://www.apachefriends.org/xampp-files/8.1.2/xampp-linux-x64-8.1.2-0-installer.run
 sudo chmod 755 xampp-linux-x64-8.1.2-0-installer.run
+sudo ./xampp-linux-x64-8.1.2-0-installer.run
 Y
 Y
 ENTER

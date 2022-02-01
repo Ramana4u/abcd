bc05468c08858102ca1fcde359f5ff974254a0f4
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-02-01 10:41:58 +0530

    Update terraformec2 (1).tf

--- terraformec2 (1).tf
+++ terraformec2 (1).tf
@@ -28,3 +28,42 @@ resource "aws_db_instance" "default" {
   publicly_accessible  = true
   vpc_security_group_ids = ["sg-017c097bb1674f881"]
 }
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = var.name_prefix
+  image_id      = var.image_id
+  instance_type = var.instance_type
+  security_groups    = var.security_groups
+  user_data = "${file("userdata.sh")}"
+}
+resource "aws_autoscaling_group" "bar" {
+  name                 = var.name 
+  depends_on           = ["aws_launch_configuration.as_conf"]
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 2
+  desired_capacity     = 1
+  target_group_arns   = [var.target_group_arns]
+ availability_zones = var.availability_zones
+}
+resource "aws_lb" "test" {
+  name               = var.name_lb
+  internal           = false
+  load_balancer_type = var.load_balancer_type
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+}
+resource "aws_lb_target_group" "test" {
+  name     = var.name_TG
+  port     = 80
+  protocol = var.protocol
+  vpc_id   = var.vpc_id
+}
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = var.protocol
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
